f300ae18e88a5c60f8f2a5b2622dc662a46bd849
Faza Ghani Irham <fghaniirham1@gmail.com>
2019-12-06 20:15:48 +0700

    [RED] Unit Test App 1 (Homepage)

---
+++ app1/tests.py
@@ -0,0 +1,29 @@
+from django.test import TestCase, Client
+from django.urls import resolve
+from .views import index
+
+# Create your tests here.
+class UnitTest(TestCase):
+    def test_buka_homepage(self):
+        response = Client().get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_pake_fungsi_index(self):
+        found = resolve('/')
+        self.assertEqual(found.func, index)
+
+    def test_ada_navbar(self):
+        response = Client().get('')
+        self.assertContains(response, '<nav')
+    
+    def test_ada_footer(self):
+        response = Client().get('')
+        self.assertContains(response, '</footer>')
+
+    def test_ada_nama_DNA(self):
+        response = Client().get('')
+        self.assertContains(response, 'DNA')
+    
+    def test_ada_gambar(self):
+        response = Client().get('')
+        self.assertContains(response, 'image.png')
