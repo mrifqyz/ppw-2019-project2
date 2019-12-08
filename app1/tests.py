8e2824dea8ada606119437dd52b531eb883c131e
Faza Ghani Irham <fghaniirham1@gmail.com>
2019-12-08 16:37:27 +0700

    [RED] Add Unit Test App1

--- app1/tests.py
+++ app1/tests.py
@@ -8,34 +8,52 @@ class UnitTest(TestCase):
         response = Client().get('')
         self.assertEqual(response.status_code, 200)
 
+    def test_buka_homepage_2(self):
+        response = Client().get('/home')
+        self.assertEqual(response.status_code, 200)
+
     def test_homepage_pake_fungsi_index(self):
         found = resolve('/')
         self.assertEqual(found.func, index)
 
-    def test_ada_navbar(self):
+    def test_buka_halaman_bantuan(self):
+        response = Client().get('/bantuan')
+        self.assertEqual(response.status_code, 200)
+
+    def test_buka_halaman_bantuan_pakai_fungsi_bantuan(self):
+        found = resolve('/bantuan')
+        self.assertEqual(found.func, bantuan)
+
+    def test_ada_navbar_di_homepage(self):
         response = Client().get('')
         self.assertContains(response, '<nav')
     
-    def test_ada_footer(self):
+    def test_ada_footer_di_homepage(self):
         response = Client().get('')
         self.assertContains(response, '</footer>')
 
-    def test_ada_nama_DNA(self):
+    def test_ada_nama_DNA_di_homepage(self):
         response = Client().get('')
         self.assertContains(response, 'DNA')
     
-    def test_ada_gambar(self):
+    def test_ada_gambar_di_homepage(self):
         response = Client().get('')
         self.assertContains(response, 'image.png')
 
-    def test_ada_button_sign_up(self):
+    def test_ada_button_sign_up_di_homepage(self):
         response = Client().get('')
         self.assertContains(response, '</button>')
 
-    def test_ada_ajakan_ngedanus(self):
+    def test_ada_ajakan_ngedanus_di_homepage(self):
         response = Client().get('')
         self.assertContains(response, 'Yuk bergabung dengan kami')
 
     def test_ada_deskripsi_DNA(self):
         response = Client().get('')
         self.assertContains(response, 'Sebuah platform buat kamu')
+
+    def test_bisa_buat_model_bantuan(self):
+        response_post = Client().post('/submit', {'pertanyaan' : 'Apa?',
+                                                  'jawaban' : 'Iya',
+                                                  'nama' : 'Ahmad'})
+        self.assertEqual(response_post.status_code, 302)
