aedf86a410d739d355df8092e19ef30173ad8aae
mrifqyz17 <rifqy.lxgw@gmail.com>
2019-12-12 23:03:46 +0700

    [GREEN] change setting py

--- ppwProject2/settings.py
+++ ppwProject2/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -143,5 +143,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_URL = '/login'
 
 if os.environ.get('ENVIRONMENT') == 'PRODUCTION':
-    django_heroku.settings(locals())
-
+    #django_heroku.settings(locals())
+    DATABASES['default'] = dj_database_url.config()
