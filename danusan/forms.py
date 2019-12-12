b17b5fe76d2843fb80981e37b1b5d3694590549b
Rifqy Zulkarnaen <rifqy.lxgw@gmail.com>
2019-12-12 14:41:48 +0000

    Update forms.py

--- danusan/forms.py
+++ danusan/forms.py
@@ -5,58 +5,19 @@ from .models import Danusan
 
 class DanusanForm(ModelForm):
     class Meta:
-	model = Danusan
-	fields = ['name', 'price', 'image']
+	    model = Danusan
+	    fields = ['name', 'price', 'image']
 		widgets = {
-		'name': forms.TextInput(attrs={
-		'class': 'form-control',
-		'id': 'name'}),
-		'price': forms.TextInput(attrs={
-		'type' : 'number',
-		'class': 'form-control',
-		'id': 'price'}),
-        image': forms.TextInput(attrs={
-        'class': 'form-control',
-        'id': 'image'}),
-	}
-    # class Meta:
-    #     model = Danusan
-    #     fields = ['name', 'price', 'image']
-    #     widgets = {
-    #         'name': forms.TextInput(attrs={
-    #             'class': 'form-control form-custom my-1',
-    #             'placeholder': 'Nama Danusan'}),
-    #         'price': forms.TextInput(attrs={
-    #             'type': 'number',
-    #             'class': 'form-control form-custom my-1',
-    #             'placeholder': 'Harga Danusan'}),
-    #         'image': forms.TextInput(attrs={
-    #             'class': 'form-control form-custom my-1',
-    #             'placeholder': 'Link Gambar Danusan'})
-    #     }
-
-
-# class DanusanForm(forms.Form):
-#     nama = forms.CharField(widget=forms.TextInput(
-#         attrs={
-#             'class': 'form-control form-custom my-1',
-#             'placeholder': 'Nama Danusan'
-#         }
-#     ))
-
-#     harga = forms.CharField(widget=forms.NumberInput(
-#         attrs={
-#             'type': 'number',
-#             'class': 'form-control form-custom my-1',
-#             'placeholder': 'Hargas Danusan'
-#         }
-#     ))
-
-#     image = forms.CharField(widget=forms.TextInput(
-#         attrs={
-#             'class': 'form-control form-custom my-1',
-#             'placeholder': 'Link Gambar Danusan'
-#         }
-#     ))
+		    'name': forms.TextInput(attrs={
+		    'class': 'form-control',
+		    'id': 'name'}),
+		    'price': forms.TextInput(attrs={
+		    'type' : 'number',
+		    'class': 'form-control',
+		    'id': 'price'}),
+            'image': forms.TextInput(attrs={
+            'class': 'form-control',
+            'id': 'image'}),
+	    }
 
 
