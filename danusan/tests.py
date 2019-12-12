6a2bd272651f28efbc120cd27bcd88372da7bf87
mrifqyz17 <rifqy.lxgw@gmail.com>
2019-12-12 20:17:11 +0700

    [GREEN] update danusan app

--- danusan/tests.py
+++ danusan/tests.py
@@ -7,37 +7,34 @@ from userApp.models import User
 class UnitTest(TestCase):
 
 	def test_apakah_ada_url_slash(self):
-		c = Client()
-		response = c.get('/danusan/')
+		response = self.client.get('/danusan/')
 		self.assertEqual(response.status_code, 200)
 
-	def test_apakah_ada_danusan(self):		
-		c = Client()
-		response = c.get('/danusan/')
+	def test_apakah_ada_danusan(self):
+		response = self.client.get('/danusan/')
 		content = response.content.decode('utf8')
 		self.assertIn('Danusan', content)
 
 	def test_apakah_ada_table(self):
-		c = Client()
-		response = c.get('/danusan/')
+		response = self.client.get('/danusan/')
 		content = response.content.decode('utf8')
 		self.assertIn("<table", content)
 
 	def test_apakah_ada_input_submit(self):
-		c = Client()
-		response = c.get('/danusan/')
+		response = self.client.get('/danusan/')
 		content = response.content.decode('utf8')
 		self.assertIn("<input", content)
 		self.assertIn("submit", content)
 
 	def setUp(self):
 		self.user = User.objects.create_user('mohamad.rifqy@ui.ac.id', 'Mohamad Rifqy Zulkarnaen',
-        'Tes satu satu hahaha', '087777535288', 'ssrifdiza')
+		'Tes satu satu hahaha', '087777535288', 'ssrifdiza')
+		self.client.login(email='mohamad.rifqy@ui.ac.id', password='ssrifdiza')
 
-	# def test_apakah_ada_model_danusan_dengan_field_image_name_price_dan_user(self):
-	def test_apakah_ada_model_danusan_dengan_field_image_name_dan_price(self):
-		# Danusan(image="link", name="Bakso", price="15000", user=self.user).save()
-		Danusan(image="link", name="Bakso", price="15000").save()
+	def test_apakah_ada_model_danusan_dengan_field_image_name_price_dan_user(self):
+	# def test_apakah_ada_model_danusan_dengan_field_image_name_dan_price(self):
+		Danusan(image="link", name="Bakso", price="15000", user=self.user).save()
+		# Danusan(image="link", name="Bakso", price="15000").save()
 		all_danusan = Danusan.objects.all()
 		self.assertEqual(all_danusan.count(), 1)
 		self.assertEqual(Danusan.objects.get(id=1).name, "Bakso")
@@ -59,13 +56,10 @@ class FunctionalTest(StaticLiveServerTestCase):
 		self.browser.get(self.live_server_url + "/danusan/")
 
 		user = User.objects.create_user('mohamad.rifqy@ui.ac.id', 'Mohamad Rifqy Zulkarnaen',
-        'Tes satu satu hahaha', '087777535288', 'ssrifdiza')
+		'Tes satu satu hahaha', '087777535288', 'ssrifdiza')
 
-	# def test_secure_page(self):
-	# 	self.client.login(username='temporary', password='temporary')
-	# 	response = self.client.get('/manufacturers/', follow=True)
-	# 	user = User.objects.get(username='temporary')
-	# 	self.assertEqual(response.context['email'], 'temporary@gmail.com')
+	def test_secure_page(self):
+		self.client.login(email='mohamad.rifqy@ui.ac.id', password='ssrifdiza')
 
 	def tearDown(self):
 		self.browser.quit()
