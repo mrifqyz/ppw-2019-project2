32d8105811db2a6e4989ad3415d2e62a1e14d1b4
kezia <keziasulami.pooh@gmail.com>
2019-12-13 22:24:34 +0700

    [GREEN] Fix urls.py in Danusan app

--- danusan/urls.py
+++ danusan/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path
-from .views import index, add_danusan, get_danusan, login#, danusanJSON#, delete_danusan
+from .views import index, add_danusan, get_danusan, login, danusanJSON#, delete_danusan
 
 urlpatterns = [
 	path('', index, name='index_danusan'),
